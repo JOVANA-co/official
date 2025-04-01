8d4652c17bdd02c7ab9ca5d0a116340686c41379
李昀宸 <b09705015@ntu.edu.tw>
2025-04-02 02:58:39 +0800

    feat(app): initialize eslint & husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import tsParser from "@typescript-eslint/parser";
+import noRelativeImportPaths from "eslint-plugin-no-relative-import-paths";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,50 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "no-relative-import-paths": noRelativeImportPaths,
+      "simple-import-sort": simpleImportSort,
+    },
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      "no-relative-import-paths/no-relative-import-paths": [
+        "warn",
+        {
+          allowSameFolder: false,
+          prefix: "@",
+          rootDir: "src",
+        },
+      ],
+
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            ["^@?\\w"],
+            ["^.*\\u0000$"],
+            ["^@"],
+            ["^\\u0000"],
+            ["^.+\\.s?css$"],
+            ["^@/(static|assets)(/.*|$)"],
+          ],
+        },
+      ],
+
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
